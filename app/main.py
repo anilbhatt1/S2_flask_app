7929195e6b87734d43db0bedc0820d83f28bb3a6
anilbhatt1 <anilaknb@gmail.com>
2021-10-01 22:37:26 +0530

    Modified main, predict.html to fix image dispaly issue

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from app.torch_utils import transform_image, get_prediction
+#from torch_utils import transform_image, get_prediction
 from flask import render_template, flash, redirect, url_for
 import os
 import io
+import base64
 from PIL import Image
 
 app = Flask(__name__)
@@ -21,11 +23,11 @@ def predict():
     if request.method == 'POST':
         file = request.files.get('image')
         if file is None or file.filename == "":
-            data = {'error':'no file'}
-            return render_template("predict.html", predicted_dict=data)
+            out_data = {'error':'no file'}
+            return render_template("predict.html", predicted_dict=out_data)
         if not allowed_file(file.filename):
-            data = {'error':' Uploaded image format not supported'}
-            return render_template("predict.html", predicted_dict=data)
+            out_data = {'error':' Uploaded image format not supported'}
+            return render_template("predict.html", predicted_dict=out_data)
 
         #try:
         file_name = file.filename
@@ -33,7 +35,7 @@ def predict():
         tensor, pil_img = transform_image(img_bytes)
         prediction = get_prediction(tensor)
         class_idx = int((prediction[0][0]))
-        data = {'prediction': class_idx, 'class_name': cifar10_dict[class_idx]}
+        out_data = {'prediction': class_idx, 'class_name': cifar10_dict[class_idx]}
         cwd = os.getcwd()
         print('cwd:', cwd)
         print('Directories:', os.listdir('.'))
@@ -41,15 +43,20 @@ def predict():
         image_path = os.path.join(cwd, file_name)
         print('image_path:', image_path)
         im = Image.open(io.BytesIO(img_bytes))
-        im.save(image_path)
+        data = io.BytesIO()
+        #im.save(image_path)
+        im.save(data, 'JPEG')
+        encoded_img_data = base64.b64encode(data.getvalue())
+
         print('File Created or not in :', image_path, 'Answer:', os.path.exists(image_path))
 
         disp_img_path = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
         print('disp_image_path:', disp_img_path)
-        return render_template("predict.html", predicted_dict=data, image_path=image_path)
+        #return render_template("predict.html", predicted_dict=data, image_path=image_path)
+        return render_template("predict.html", predicted_dict=out_data, img_data=encoded_img_data.decode('utf-8'))
         #except:
-        #    data = {'error':'error during prediction'}
-        #    return render_template("predict.html", predicted_dict=data)
+        #    out_data = {'error':'error during prediction'}
+        #    return render_template("predict.html", predicted_dict=out_data)
     else:
         return render_template("predict.html")
 
