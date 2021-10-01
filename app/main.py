9a93078639d6838e7d78d5ad34ba00d36767c4a1
anilbhatt1 <anilaknb@gmail.com>
2021-10-01 20:20:37 +0530

    Initial Commit

---
+++ app/main.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, jsonify
+from app.torch_utils import transform_image, get_prediction
+from flask import render_template, flash, redirect, url_for
+import os
+import io
+from PIL import Image
+
+app = Flask(__name__)
+
+image_folder = os.path.join('static', 'saved_images')
+app.config['UPLOAD_FOLDER'] = image_folder
+
+ALLOWED_EXTENSIONS = {'png','jpg', 'jpeg'}
+cifar10_dict = {0:'plane', 1:'automob', 2:'bird', 3:'cat',4:'deer', 5:'dog', 6:'frog', 7:'horse', 8:'ship', 9:'truck'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET','POST'])
+def predict():
+    if request.method == 'POST':
+        file = request.files.get('image')
+        if file is None or file.filename == "":
+            data = {'error':'no file'}
+            return render_template("predict.html", predicted_dict=data)
+        if not allowed_file(file.filename):
+            data = {'error':' Uploaded image format not supported'}
+            return render_template("predict.html", predicted_dict=data)
+
+        try:
+            file_name = file.filename
+            img_bytes = file.read()
+            tensor, pil_img = transform_image(img_bytes)
+            prediction = get_prediction(tensor)
+            class_idx = int((prediction[0][0]))
+            data = {'prediction': class_idx, 'class_name': cifar10_dict[class_idx]}
+
+            image_path = os.path.join('static', 'saved_images', file_name)
+            im = Image.open(io.BytesIO(img_bytes))
+            im.save(image_path)
+
+            disp_img_path = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
+            return render_template("predict.html", predicted_dict=data, image_path=disp_img_path)
+        except:
+            data = {'error':'error during prediction'}
+            return render_template("predict.html", predicted_dict=data)
+    else:
+        return render_template("predict.html")
+
+
---
+++ app/torch_utils.py
@@ -0,0 +1,33 @@
+import io
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+from google_drive_downloader import GoogleDriveDownloader as gdd
+from pathlib import Path
+
+channels_mean  = [0.49139968, 0.48215841, 0.44653091]
+channels_stdev = [0.24703223, 0.24348513, 0.26158784]
+
+def transform_image(image_bytes):
+    transform = transforms.Compose([transforms.Resize((32, 32)),
+                                    transforms.ToTensor(),
+                                    transforms.Normalize(mean=channels_mean, std=channels_stdev),
+                                    ])
+    image = Image.open(io.BytesIO(image_bytes))
+    t_img = transform(image).unsqueeze(0)
+    return t_img, image
+
+def get_prediction(image_tensor):
+    model_path = Path("./cifar10_resnet18.pt")
+    if not model_path.exists():
+        gdd.download_file_from_google_drive(file_id='1--JIPQ_1XMQuPanEu4cWdHH0Qw0yvvBY',
+                                            dest_path='./cifar10_resnet18.pt', unzip=False)
+    model = torch.jit.load('./cifar10_resnet18.pt')
+    model.eval()
+    with torch.no_grad():
+        labels_pred: Tensor = model(image_tensor)
+        labels_pred_max = labels_pred.argmax(dim=1, keepdim=True)
+    labels_pred_max = labels_pred_max.cpu().numpy()
+    return labels_pred_max
+
+
